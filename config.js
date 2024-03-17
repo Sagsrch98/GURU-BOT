db0142316043ed60cfd4ee44f4200878484620bc
Sagsrch98 <143768770+Sagsrch98@users.noreply.github.com>
2024-03-17 08:33:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ global.owner = [
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "923400940198" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['923400940198','32460248586'] 
+global.prems = ['923400940198', '923400940198', '923400940198']
+global.allowed = ['923400940198', '923400940198', '923400940198']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
